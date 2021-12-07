5b48bc721b5aadddcf5576905731a3127d8ca28a
scottdiemer <sdiemer@cfl.rr.com>
2021-12-06 22:07:57 -0500

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+function AboutPage() {
+  return (
+    <>
+      <main>
+        <h1>About</h1>
+      </main>
+    </>
+  );
+}
+
+export default AboutPage;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <>
+      <main>
+        <h1>My Blog</h1>
+      </main>
+    </>
+  );
+}
+
+export default HomePage;
